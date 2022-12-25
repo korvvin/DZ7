3f50100f28f64d7b6c67c5b90173ba3b201c5927
zykov <zykov.da01@gmail.com>
2022-12-26 00:09:37 +0300

    DZ

--- src/pro/sky/java/course1/lesson7/Books.java
+++ src/pro/sky/java/course1/lesson7/Books.java
@@ -1,5 +1,7 @@
 package pro.sky.java.course1.lesson7;
 
+import java.util.Objects;
+
 public class Books {
     private String name;
     private Author author;
@@ -23,5 +25,23 @@ public class Books {
     }
     public void setYear(int year) {
         this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return name+" "+year+" "+author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Books books = (Books) o;
+        return year == books.year && Objects.equals(name, books.name) && Objects.equals(author, books.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, author, year);
     }
 }
